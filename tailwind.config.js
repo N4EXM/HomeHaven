513f3c647f077e80436fea9a0b0fb369ad2b4b22
N4EXM <naemmmail89@gmail.com>
2025-07-02 19:21:52 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",  // Scan all HTML files in public/
+    "./src/**/*.js",       // Scan JS files if needed
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "amenableBlue": "#d7ebf7",
+        "swimmingPool": "#73c3ff",
+        "tearDrop": "#0068ef",
+        "blueHour": "#004187",
+        "blueBerry": "#002854",
+        "deepSea": "#001b32",
+        "darkBackground": "#111827",
+        "secondDarkBackground": "#000b1d" 
+      }
+    },
+  },
+  plugins: [],
+}
+
