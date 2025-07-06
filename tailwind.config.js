7bffa173b804fe540ba51c85cb1cd13a13317c3b
N4EXM <naemmmail89@gmail.com>
2025-07-06 14:46:36 +0100

    changed the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,6 @@ module.exports = {
     "./src/**/*.js",       // Scan JS files if needed
   ],
   theme: {
-    screens: {
-      "sm": "360px",
-      "md": "480px",
-      "lg": "768px",
-      "xl": "1024px",
-      "2xl": "1280px"
-    },
     extend: {
       colors: {
         "amenableBlue": "#d7ebf7",
